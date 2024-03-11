044c5a3f7960660f85f85302a7182cc7836d8d68
buyekeobare <buyekeobare@gmail.com>
2024-03-12 00:00:12 +0300

    Updated files

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,17 +7,17 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *down = list;
-	listint_t *up = list;
+	listint_t *slow = list;
+	listint_t fast = list;
 
 	if (!list)
 		return (0);
 
-	while (down && up && up->next)
+	while (slow && fast && fast->next)
 	{
-		down = down->next;
-		up = up->next->next;
-		if (down == up)
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 			return (1);
 	}
 
