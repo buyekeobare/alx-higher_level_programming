717a8393f37deb686008691a0503a6c52bcf08dd
buyekeobare <buyekeobare@gmail.com>
2024-03-14 09:08:14 -0700

    Updated file

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    print(a)
