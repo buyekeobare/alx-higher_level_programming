f4e8a99dc2f426e7fed5777baa32f7de405ae356
buyekeobare <buyekeobare@gmail.com>
2024-04-08 12:58:10 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""Establishes a Rectangle class."""
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """Signifies a rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
+        """This initializes a new Rectangle.
         Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
+            width (int): Width of the new rectangle.
+            height (int): Height of the new rectangle.
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Get/set the width of the rectangle."""
+        """Retrieve/set the width of the rectangle."""
         return self.__width
 
     @width.setter
@@ -29,7 +29,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set the height of the rectangle."""
+        """Retrieve/set the height of the rectangle."""
         return self.__height
 
     @height.setter
