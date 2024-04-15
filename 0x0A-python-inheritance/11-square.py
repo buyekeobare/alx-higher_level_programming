257d2f0d7717d31f006256e0c9a867e2f4d94d7e
buyekeobare <buyekeobare@gmail.com>
2024-04-15 11:40:48 -0700

    Files created

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""this module defines a Rectangle subclass Square"""
+"""This module defines a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square"""
+    """Represent a square."""
 
     def __init__(self, size):
-        """Initialize a new square
+        """Initialize a new square.
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Inheris from baseGeometry"""
+"""Inheris from baseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
@@ -7,7 +7,7 @@ class Rectangle(BaseGeometry):
     """a class to define rectangle using BaseGeometry"""
 
     def __init__(self, width, height):
-        """Intialize a new Rectangle
+        """Intialize a new Rectangle.
         """
         self.integer_validator("width", width)
         self.__width = width
