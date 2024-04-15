1ce0275449ee20e0b86cd0bbc91945f9d405ea29
buyekeobare <buyekeobare@gmail.com>
2024-04-15 09:57:43 -0700

    Files created

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module inherits from the list class"""
+
+
+class MyList(list):
+    """A class that inherits from the list"""
+    def print_sorted(self):t
+        """prints a sorted list"""
+        print(sorted(self))
