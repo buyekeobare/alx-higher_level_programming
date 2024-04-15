a52c5e476ebaf2e202b7157d4661ab7dad76354c
buyekeobare <buyekeobare@gmail.com>
2024-04-15 11:53:00 -0700

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-"""  
-A function that returns True
-if the object is an instance of a class that inherited
-(directly or indirectly) from the specified class;
-otherwise False
-"""
+
+"""The module is 4-inherits_from"""
 
 
 def inherits_from(obj, a_class):
-    """ The function
-    Args:
-    obj: object in question
-    a_class: class checked
     """
-    if issubclass(type(obj), a_class) and type(obj) is not a_class:
-        return True
-    return False
+    a function that returns True if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
