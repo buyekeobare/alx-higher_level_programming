c1396f509dbd59c8c5f6c8416ffa6c796759dc07
buyekeobare <buyekeobare@gmail.com>
2024-04-22 11:52:55 -0700

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,174 +1,163 @@
 #!/usr/bin/python3
-"""
-    This is a class Rectangle that implements Base.
+""" My first class
+Rectangle
 """
 
+
 from models.base import Base
+""" importing from the super class
+"""
 
 
 class Rectangle(Base):
+    """ instantiation of the Rectangle class
     """
-        class Rectangle implements Base.
-        Methods:
-            __init__()
-    """
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ The base class constructor
+        method
         """
-            Initializes the instance of the class Rectangle..
-        """
-        super().__init__(id)
+
+        # if not isinstance(width, int):
+        #     raise TypeError("width must be an integer")
+        # if not isinstance(height, int):
+        #     raise TypeError("height must be an integer")
+        # if width <= 0:
+        #     raise ValueError("width must be > 0")
+        # if height <= 0:
+        #     raise ValueError("width must be > 0")
         self.width = width
         self.height = height
+        # if not isinstance(x, int):
+        #     raise TypeError("x must be an integer")
+        # if not isinstance(y, int):
+        #     raise TypeError("y must be an integer")
+        # if x < 0:
+        #     raise ValueError("x must be >= 0")
+        # if y < 0:
+        #     raise ValueError("y must be >= 0")
+
         self.x = x
         self.y = y
+        super().__init__(id)
 
     @property
     def width(self):
-        """
-            retriever function for __width
-            Returns: width
-        """
+        """private getter width"""
+
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-            setter function for width.
-            Args:
-                value (int): value to be set.
-        """
-        if type(value) != int:
+        """private setter width"""
+
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-
         self.__width = value
 
     @property
     def height(self):
-        """
-            retriever function for height
-            Returns: height
-        """
+        """private getter height"""
+
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-            setter function for height
-            Args:
-                value (int): value to be set.
-        """
-        if type(value) != int:
+        """private setter height"""
+
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
-
         self.__height = value
 
     @property
     def x(self):
-        """
-            retriever function for x.
-            Returns: x
-        """
+        """private getter x"""
+
         return self.__x
 
     @x.setter
     def x(self, value):
-        """
-            setter function for x.
-            Args:
-                value (int): value to be set.
-        """
-        if type(value) != int:
+        """private setter x"""
+
+        if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
-
         self.__x = value
 
     @property
     def y(self):
-        """
-            retriever function for y
-            Returns: y
-        """
+        """private getter y"""
+
         return self.__y
 
     @y.setter
     def y(self, value):
-        """
-            setter function for y
-            Args:
-                value (int): value to be set.
-        """
-        if type(value) != int:
+        """private setter y"""
+
+        if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
-
         self.__y = value
 
     def area(self):
-        """
-            returns the area of the class Rectangle instance.
-        """
-        return (self.__width * self.__height)
+        """ width * height """
+
+        return self.__width * self.__height
 
     def display(self):
-        """
-            prints to stdout the class Rectangle instance with '#'
-        """
-        rectangle = ""
-        print_symbol = "#"
+        """ printing to stdout """
 
-#        for i in range(self.__height - 1):
-#            rectangle += print_symbol * self.__width + "\n"
-#        rectangle += print_symbol * self.__width
-
-#        print("{}".format(rectangle))
-
-        print("\n" * self.y, end="")
-
-        for i in range(self.height):
-            rectangle += (" " * self.x) + (print_symbol*self.width) + "\n"
-        print(rectangle, end="")
+        for m in range(self.__y):
+            print()
+        for i in range(self.__height):
+            for n in range(self.__x):
+                print(" ", end="")
+            for j in range(self.__width):
+                print("#", end="")
+            print()
 
     def __str__(self):
-        """
-            returns a str formart of the class Rectangle
-        """
-        return "[{}] ({}) {}/{} - {}/{}".format(type(self).__name__, self.id,
-                                                self.__x, self.__y,
-                                                self.__width, self.__height)
+        """ displays text to stdout in format """
+
+        return (
+                "[Rectangle] ({}) {}/{} - {}/{}".format(
+                    self.id, self.__x, self.__y, self.__width, self.__height)
+                )
 
     def update(self, *args, **kwargs):
-        """
-            assigns key/value argument to attributes
-            kwargs is skipped if args is not empty
-            Args:
-                *args -  variable number of no-keyword args
-                **kwargs - variable number of keyworded args
-        """
-        if len(args) == 0:
-            for key, val in kwargs.items():
-                self.__setattr__(key, val)
-            return
+        """ assigns an argument to each attribute """
 
-        try:
-            self.id = args[0]
-            self.width = args[1]
-            self.height = args[2]
-            self.x = args[3]
-            self.y = args[4]
-        except IndexError:
-            pass
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def to_dictionary(self):
+        """ Returns the dic representation of
+        a rect
         """
-            returns the dictionary repr of a class Rect
-        """
-        return {'x': getattr(self, "x"), 'y': getattr(self, "y"),
-                'id': getattr(self, "id"), 'height': getattr(self, "height"),
-                'width': getattr(self, "width")}
+
+        return {
+                'id': self.id,
+                'width': self.__width,
+                'height': self.__height,
+                'x': self.__x,
+                'y': self.__y
+                }
+
