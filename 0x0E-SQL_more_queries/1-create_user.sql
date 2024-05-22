0a55bd0856d7cb375e368c587feebddd7ada99f6
buyekeobare <buyekeobare@gmail.com>
2024-05-22 12:34:59 -0700

    Files created

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- This the script that creates the MySQL server user
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
