e02bbe30b22cf4c17d68c839d0f5d6f97f4981dd
buyekeobare <buyekeobare@gmail.com>
2024-06-17 13:29:23 -0700

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,18 +4,18 @@ const process = require('process');
 const argOne = process.argv[2];
 const num = parseInt(argOne);
 
-function factorial (x) {
+function calcFactorial (x) {
   if (isNaN(x)) {
     return 1;
   } else if (x === 0) {
     return 1;
   } else {
-    return x * factorial(x - 1);
+    return x * calcFactorial(x - 1);
   }
 }
 
 if (!isNaN(num)) {
-  console.log(factorial(num));
+  console.log(calcFactorial(num));
 } else {
   console.log('1');
 }
