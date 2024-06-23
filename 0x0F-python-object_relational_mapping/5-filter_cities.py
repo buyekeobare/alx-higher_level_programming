e4d0e9c7fc5811d9274c5273156056cc00782ccd
buyekeobare <buyekeobare@gmail.com>
2024-06-23 01:59:36 -0700

    5-filter_cities.py updated script

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""
+This miodule that connects python script to a database.
+This script lists all states with a name starting with N.
+Script should take 3 arguments:
+mysql username, mysql password and database name
+script should connect to a MySQL server running on localhost at port 3306
+"""
+
+import MySQLdb as db
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    Access to the database and get the cities
+    from the database.
+    """
+
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+
+    with db_connect.cursor() as db_cursor:
+        db_cursor.execute("""
+            SELECT
+                cities.id, cities.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            WHERE
+                states.name LIKE BINARY %(state_name)s
+            ORDER BY
+                cities.id ASC
+        """, {
+            'state_name': argv[4]
+        })
+        rows_selected = db_cursor.fetchall()
+
+    if rows_selected is not None:
+        print(", ".join([row[1] for row in rows_selected]))
