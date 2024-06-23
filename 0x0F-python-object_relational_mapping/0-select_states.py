00e1a3c3a6b37fa169c292b3598c47fb565707d0
buyekeobare <buyekeobare@gmail.com>
2024-06-23 00:24:56 -0700

    Updated script

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,23 +10,23 @@ This module connects a python script to a database
     if __name__ == "__main__":
 
     # Connect database using command-line args
-    db_db = MySQLdb.connect(host='localhost', user=argv[1], password=argv[2],
+    db = MySQLdb.connect(host='localhost', user=argv[1], password=argv[2],
                             db=argv[3], port=3306)
     # Create cursor object to interact with db
-    db_cursor = db_db.cursor()
+    cur = db.cursor()
 
     # Execute a SELECT query to fetch data
-    db_cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
 
     # fetch all the data returned by the query
-    db_data = db_cursor.fetchall()
+    data = cur.fetchall()
 
     # Iterate through the fetched data and print each row
-    for row in db_data:
+    for row in data:
         print(row)
 
     # Close all cursors
-    db_cursor.close()
+    cur.close()
 
     # Close all databases
-    db_db.close()
+    db.close()
