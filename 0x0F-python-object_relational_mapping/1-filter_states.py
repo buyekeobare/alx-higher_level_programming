877ebe17c3ce5455f26c4ab6329a21ddc311c431
buyekeobare <buyekeobare@gmail.com>
2024-06-23 01:21:26 -0700

    Updated script

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,19 +7,21 @@ Script should take 3 arguments:
 mysql username, mysql password and database name
 script should connect to a MySQL server running on localhost at port 3306
 """
-if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
 
-    my_db_connect = my_db.connect(host="localhost", port=3306,
+import MySQLdb as db
+from sys import argv
+
+
+if __name__ == '__main__':
+    db_connect = db.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
-    my_db_cursor = my_db_connect.cursor()
+    db_cursor = db_connect.cursor()
 
-    my_db_cursor.execute(
+    db_cursor.execute(
         "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
                 ORDER BY states.id ASC")
 
-    rows_selected = my_db_cursor.fetchall()
+    rows_selected = db_cursor.fetchall()
 
     for row in rows_selected:
-        print(row) 
+        print(row)
