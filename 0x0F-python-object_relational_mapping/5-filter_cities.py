c81e4b1371c1372a626cb88ee31ce0213e1782cf
buyekeobare <buyekeobare@gmail.com>
2024-06-23 02:17:15 -0700

    5-filter_cities.py updated script

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,16 +12,12 @@ import MySQLdb as db
 from sys import argv
 
 if __name__ == "__main__":
-    """
-    Access to the database and get the cities
-    from the database.
-    """
 
-    db_connect = db.connect(host="localhost", port=3306,
+    my_db = db.connect(host="localhost", port=3306,
                             user=argv[1], passwd=argv[2], db=argv[3])
 
-    with db_connect.cursor() as db_cursor:
-        db_cursor.execute("""
+    with my_db.cursor() as my_cursor:
+        my_cursor.execute("""
             SELECT
                 cities.id, cities.name
             FROM
@@ -37,7 +33,7 @@ if __name__ == "__main__":
         """, {
             'state_name': argv[4]
         })
-        rows_selected = db_cursor.fetchall()
+        rows_selected = my_cursor.fetchall()
 
     if rows_selected is not None:
         print(", ".join([row[1] for row in rows_selected]))
