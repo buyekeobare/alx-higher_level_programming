45acd619b135c9f21d0306e6ee20063fd3164e9d
buyekeobare <buyekeobare@gmail.com>
2024-07-05 13:05:35 -0700

    6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Sends a POST request to the passed URL with the email as a parameter."""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = {'email': email}
+
+    response = requests.post(url, data=data)
+    print(response.text)
