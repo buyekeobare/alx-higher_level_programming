c05fda349212d9eae1ed5d213d336e2d4cb5c90e
buyekeobare <buyekeobare@gmail.com>
2024-07-30 10:43:23 -0700

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// Print the number of movies where the character “Wedge Antilles” is present
+// print the number of movies where the character “Wedge Antilles” is present
 const request = require('request');
 const url = process.argv[2];
 const actor = 'https://swapi-api.alx-tools.com/api/people/18/';
