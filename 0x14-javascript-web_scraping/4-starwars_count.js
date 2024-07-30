2b02d1ef6f6a634418e6a2f6e15cd01782368108
buyekeobare <buyekeobare@gmail.com>
2024-07-30 10:48:07 -0700

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,24 @@
 #!/usr/bin/node
 // print the number of movies where the character “Wedge Antilles” is present
-const request = require('request');
-const url = process.argv[2];
-const actor = 'https://swapi-api.alx-tools.com/api/people/18/';
 
-request(url, (err, res, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    const films = JSON.parse(data).results;
-    const match = films.filter((film) => film.characters.includes(actor));
-    console.log(`${match.length}`);
+// imports the module
+const request = require('request');
+
+// The first argument is the API URL
+const apiUrl = process.argv[2];
+
+// Makes an HTTP GET request to the API
+request(apiUrl, function (error, response, body) {
+  if (!error) {
+    // parses the JSON response
+    const results = JSON.parse(body).results;
+
+    // counts no of movies where "Wedge Antilles" is present
+    const moviesWithWedge = results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1 // if found
+        : count; // if not found
+    }, 0); // initiliazing count to zero
+    console.log(moviesWithWedge);
   }
 });
